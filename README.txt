bdbd9ac86f4959d81b2b59f86b6724a29696aedd
Bailey Malone <malone.bailey@gmail.com>
2012-06-26 10:38:03 -0600

    First commit, README and Satellite.py

---
+++ README.txt
@@ -0,0 +1,2 @@
+A GPS simulation with an assumed spherical Earth.
+Code written in C++ with embedded Python.
---
+++ Satellite.py
@@ -0,0 +1,24 @@
+'''
+Created on Jun 23, 2012
+
+@author: Bailey Malone
+'''
+
+class Satellite(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
+    # Returns true if the Satellite is above the horizon @ 
+    # Xv, false otherwise.
+    def isAboveHorizon(self):
+        
+        
+    def computeXv(self, double phi, double lamda):
+        
